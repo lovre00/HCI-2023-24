7cb89702f955a8a0da142a03e36fa4283ea9b7ae
lgrada00 <lgrada00@fesb.hr>
2023-10-25 16:32:15 +0200

    Add next.js app

---
+++ semester-project/my-app/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+    return (
+      <nav className={styles.navbar}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link href="/">
+              <h2 className={styles.navLink}>Home</h2>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/about">
+              <h2 className={styles.navLink}>About</h2>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/contact">
+              <h2 className={styles.navLink}>Contact</h2>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+export default Navbar;
